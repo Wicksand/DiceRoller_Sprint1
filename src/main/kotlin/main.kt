0f7d0d6f3683e50e7a13ad970225f3a04f7b03aa
Wicksand <wicksand@byui.edu>
2026-05-07 02:15:16 -0600

    New Repo so that it doesn't have all of my messy practice stuff

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,4 +7,12 @@ import com.googlecode.lanterna.TerminalSize
 fun main(){
     val screen   = DefaultTerminalFactory().createScreen()
 
+    screen.startScreen()
+
+
+    screen.refresh()
+    Thread.sleep(2000)
+    screen.stopScreen()
+
+
 }
